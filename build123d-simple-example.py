2d212608ea7b88d551912e2cbf80355258c20714
czeppi <git@czeppi.de>
2025-05-02 08:04:58 +0200

    remove old files + tests

--- build123d-simple-example.py
+++
@@ -1,9 +0,0 @@
-from build123d import BuildPart, Box
-from ocp_vscode import show_object
-
-length, width, thickness = 80.0, 60.0, 10.0
-
-with BuildPart() as the_box:
-    Box(length, width, thickness)
-
-show_object(the_box)
