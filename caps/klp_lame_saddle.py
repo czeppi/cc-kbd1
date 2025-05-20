5552e85cf98624acf9b2ce19cb6efe9aef53bc44
czeppi <git@czeppi.de>
2025-05-20 13:22:40 +0200

    klp-lame: simplified grid creator

--- caps/klp_lame_saddle.py
+++ caps/klp_lame_saddle.py
@@ -28,120 +28,125 @@ def main():
     show_object(part)
 
 
-def create_orig_cap() -> None:
+def create_orig_cap() -> Solid:
     key_cap = LameSaddleKeyCapCreator(cap_kind=CapKind.ORIG, extra_height=0.6).create()
     export_stl(key_cap, OUTPUT_DPATH / 'lame-key-cap-orig.stl')
     return key_cap
 
 
-def create_index_normal_cap() -> None:
+def create_index_normal_cap() -> Solid:
     key_cap = LameSaddleKeyCapCreator(cap_kind=CapKind.INDEX_FINGER_STD, extra_height=0.6).create()
     export_stl(key_cap, OUTPUT_DPATH / 'lame-key-cap-index-normal.stl')
     return key_cap
 
 
-def create_index_big_cap() -> None:
+def create_index_big_cap() -> Solid:
     key_cap = LameSaddleKeyCapCreator(cap_kind=CapKind.INDEX_FINGER_BIG, extra_height=0.6).create()
     export_stl(key_cap, OUTPUT_DPATH / 'lame-key-cap-index-big.stl')
     return key_cap
 
 
-def create_combined_caps() -> None:
-    return LameKeyCapGridCreator(cap_kinds_per_column=[CapKind.ORIG, 
-                                                       CapKind.INDEX_FINGER_BIG, 
-                                                       CapKind.INDEX_FINGER_STD,
-                                                       CapKind.INDEX_FINGER_CONCAVE], 
-                                 num_columns=2).create()
-
+def create_combined_caps() -> Solid:
+    cap_kind_map = {
+        'o': CapKind.ORIG,
+        'i': CapKind.INDEX_FINGER_STD,
+        'c': CapKind.INDEX_FINGER_CONCAVE,
+        'b': CapKind.INDEX_FINGER_BIG,
+        }
+    columns_data = ['ob', 'bci']
+    cap_kinds = [[cap_kind_map[ch] for ch in col_str] 
+                 for col_str in columns_data]
+    caps = LameKeyCapGridCreator(cap_kinds=cap_kinds).create()
+    export_stl(caps, OUTPUT_DPATH / 'lame-key-caps-grid2.stl')
+    return caps
 
 
 class LameKeyCapGridCreator:
 
-    
-    def __init__(self, cap_kinds_per_column: list[CapKind], num_columns: int):
-        self._cap_kinds_per_column = cap_kinds_per_column
-        self._num_columns = num_columns
+    def __init__(self, cap_kinds: list[list[CapKind]]):
+        self._cap_kinds = cap_kinds
         self._cap_map: dict[CapKind, Solid] = {}
         self._cap_box_map: dict[CapKind, BoundBox] = {}
         self._column_width: float = -1.0
 
     def create(self) -> Part:
-        cap_kindes = set(self._cap_kinds_per_column)
+        grid_data = klp_lame_data.grid
+        dist = grid_data.CAP_DISTANCE
+
+        cap_kindes = set(cap_kind for cap_kind_column in self._cap_kinds for cap_kind in cap_kind_column)
         self._cap_map = {cap_kind: LameSaddleKeyCapCreator(cap_kind=cap_kind, extra_height=0.6).create()
                          for cap_kind in cap_kindes}
         self._cap_box_map = {cap_kind: cap.bounding_box() 
                              for cap_kind, cap in self._cap_map.items()}
-        self._column_width = max(box.max.X - box.min.X for box in self._cap_box_map.values())
+        self._column_width = max(box.max.X - box.min.X 
+                                 for box in self._cap_box_map.values()) + dist
+        self._row_height = max(self._calc_row_height(box=box, cap_kind=cap_kind)
+                                for cap_kind, box in self._cap_box_map.items()) + dist
 
-        return Part() + list(self._iter_column_solids())
+        return Part() + list(self._iter_solids())
     
-    def _iter_column_solids(self) -> Iterator[Solid]:
-        grid_data = klp_lame_data.grid
-        dist = grid_data.CAP_DISTANCE
-        cap_kinds = self._cap_kinds_per_column
-        column_dist = self._column_width + dist
-        conn_cyl = self._create_column_cylinder()
-         
-        yield Part() + list(self._iter_row_solids(0))
-        #yield copy.copy(self._cap_map[cap_kinds[0]])
+    def _calc_row_height(self, box: BoundBox, cap_kind: CapKind) -> float:
+        h = box.max.Y - box.min.Y
+        if cap_kind == CapKind.INDEX_FINGER_BIG:
+            return h / 2  # big one will use 2 rows
+        else:
+            return h
+        
+    def _iter_solids(self) -> Iterator[Solid]:
+        row_cyl = self._create_row_cylinder()
+        col_cyl = self._create_column_cylinder()
 
-        prev_box = self._cap_box_map[cap_kinds[0]]
-        prev_y = 0.0
+        h = self._row_height
+        w = self._column_width
 
-        num_rows = len(self._cap_kinds_per_column)
-        for i in range(1, num_rows):
-            cur_cap_kind = self._cap_kinds_per_column[i]
-            for j in range(self._num_columns):
-                yield Pos(X=j * column_dist, Y=prev_y + prev_box.max.Y + dist/2) * copy.copy(conn_cyl)
+        prev_num_rows = 0
+        for j, cap_kind_column in enumerate(self._cap_kinds):
+            i = 0
+            for cap_kind in cap_kind_column:
+                cap = self._cap_map[cap_kind]
+                x = j * w
+                y = i * h
 
-            cur_box = self._cap_box_map[cur_cap_kind]
-            cur_y = prev_y + prev_box.max.Y + dist + abs(cur_box.min.Y)
-            row_solid = Part() + list(self._iter_row_solids(i))
-            yield Pos(Y=cur_y) * copy.copy(row_solid)
+                # horizontal conn cylinder
+                if i < prev_num_rows:
+                    yield Pos(X=x-w/2, Y=y) * copy.copy(row_cyl)
 
-            prev_box = cur_box
-            prev_y = cur_y
+                # if necessary => 2nd horizontal conn cylinder
+                if cap_kind == CapKind.INDEX_FINGER_BIG:
+                    if i + 1 < prev_num_rows:
+                        yield Pos(X=x-w/2, Y=y+h) * copy.copy(row_cyl)
+
+                # vertical conn cylinder
+                if i > 0:
+                    yield Pos(X=x, Y=y-h/2) * copy.copy(col_cyl)
+
+                # cap
+                if cap_kind == CapKind.INDEX_FINGER_BIG:
+                    y += w / 2
+
+                yield Pos(X=x, Y=y) * copy.copy(cap)
+
+                i += 2 if cap_kind == CapKind.INDEX_FINGER_BIG else 1
+
+            prev_num_rows = i
     
-    def _iter_row_solids(self, i: int) -> Iterator[Solid]:
-        cap_kind = self._cap_kinds_per_column[i]
-        cap_box = self._cap_box_map[cap_kind]
-        cap_width = cap_box.max.X - cap_box.min.X
-        cap = self._cap_map[cap_kind]
-
-        grid_data = klp_lame_data.grid
-        dist = grid_data.CAP_DISTANCE
-        column_dist = self._column_width + dist
-
-        conn_cyl_len = column_dist - cap_width + 2 * klp_lame_data.saddle.RIM_THICKNESS
-        if cap_kind == CapKind.INDEX_FINGER_CONCAVE:
-            conn_cyl_len += 1.3  # cause of the concave form
-
-        conn_cyl = self._create_row_cylinder(height=conn_cyl_len)
-
-        yield copy.copy(cap)
-
-        for j in range(1, self._num_columns):
-            x = (j - 0.5) * column_dist
-            yield Pos(X=x) * copy.copy(conn_cyl)
-
-            x = j * column_dist
-            yield Pos(X=x) * copy.copy(cap)
-
-    def _create_row_cylinder(self, height: float) -> Solid:
+    
+    def _create_row_cylinder(self) -> Solid:
         grid_data = klp_lame_data.grid
         conn_cyl_radius = grid_data.CONN_CYLINDER_RADIUS
         conn_cyl_overlap = grid_data.CONN_CYLINDER_OVERLAP_WITH_CAP
+        conn_cyl_height = grid_data.CAP_DISTANCE + 2 * klp_lame_data.saddle.RIM_THICKNESS + 2.0
 
-        return Pos(Z=-conn_cyl_radius + 1.3 + conn_cyl_overlap) * Rot(Y=90) * Cylinder(radius=conn_cyl_radius, height=height)
+        return Pos(Z=-conn_cyl_radius + 1.3 + conn_cyl_overlap) * Rot(Y=90) * Cylinder(radius=conn_cyl_radius, height=conn_cyl_height)
 
     def _create_column_cylinder(self) -> Solid:
         grid_data = klp_lame_data.grid
         conn_cyl_radius = grid_data.CONN_CYLINDER_RADIUS
         conn_cyl_overlap = grid_data.CONN_CYLINDER_OVERLAP_WITH_CAP
-        conn_cyl_height = grid_data.CAP_DISTANCE + 2 * klp_lame_data.saddle.RIM_THICKNESS
+        conn_cyl_height = grid_data.CAP_DISTANCE + 2 * klp_lame_data.saddle.RIM_THICKNESS + 4.0
 
         return Pos(Z=-conn_cyl_radius + 1.3 + conn_cyl_overlap) * Rot(X=90) * Cylinder(radius=conn_cyl_radius, height=conn_cyl_height)
-    
+
 
 class LameSaddleKeyCapCreator:
     """ 
